a2c20c7248af9d7da0110d469a885e606454029b
MinjunGao <mj.gao@hotmail.com>
2023-03-09 22:05:21 -0600

    faker

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const { faker } = require('@faker-js/faker');
 
 require('dotenv').config();
 
@@ -28,9 +29,43 @@ connection.query('SELECT CURDATE()', function (err, res, fields) {
     console.log(res);
 });
 
+// Inserting data 1
+// const qInsert = 'INSERT INTO users (email) VALUES ("uiuc@illinois.edu")';
+// connection.query(qInsert, function (error, results, fields) {
+//     if (error) throw error;
+// });
+
+// Inserting data 2
+// const person = { email: 'elonmusk@tesla.com' };
+// connection.query('INSERT INTO users SET ?', person, function (error, result) {
+//     if (error) throw error;
+// });
+
+// Insert data 3 (faker)
+// const personFaker = {
+//     email: faker.internet.email(),
+//     created_at: faker.date.past()
+// };
+// connection.query('INSERT INTO users SET ?', personFaker, function (error, result) {
+//     if (error) throw error;
+// });
+
+// Inserting 500 users (faker)
+let data = [];
+for (let i = 0; i < 500; i++) {
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+const q = 'INSERT INTO users (email, created_at) VALUES ?';
+connection.query(q, [data], function (error, result) {
+    if (error) throw error;
+});
+
 // Selecting Data
-const q = 'SELECT * FROM users';
-connection.query(q, function (error, results, fields) {
+const qSelect = 'SELECT * FROM users';
+connection.query(qSelect, function (error, results, fields) {
     if (error) throw error;
     console.log(results);
 });
--- schema.sql
+++ schema.sql
@@ -7,3 +7,6 @@ CREATE TABLE users (
 
 INSERT INTO users (email) VALUES
 ('tim@yahoo.com'), ('linda@gmail.com');
+
+-- INSERT INTO users (email, created_at) VALUES
+-- ('Julia@hotmail.com', '2022-11-17T21:06:32.078Z');
