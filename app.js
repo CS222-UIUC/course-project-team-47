035a747297a704fdd136a298f97ad83622df6951
Minjun Gao <mj.gao@hotmail.com>
2023-04-28 14:16:00 -0500

    finish CSS

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const mysql = require('mysql2');
 const { faker } = require('@faker-js/faker');
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
 
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: process.env.MYSQL_LOCAL_PORT,
     user: process.env.MYSQL_ROOT_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
 });
 
 // const connection = mysql.createConnection({
@@ -27,7 +33,7 @@ connection.connect(function (err) {
     console.log("Connected!");
 });
 
-connection.query('SELECT CURDATE()', function (err, res, fields) {
+connection.query("SELECT CURDATE()", function (err, res, fields) {
     if (err) throw err;
     console.log(res);
 });
@@ -54,7 +60,6 @@ connection.query('SELECT CURDATE()', function (err, res, fields) {
 // });
 
 // Inserting 500 users (faker)
-/*
 let data = [];
 for (let i = 0; i < 500; i++) {
     data.push([
@@ -66,7 +71,6 @@ const q = 'INSERT INTO users (email, created_at) VALUES ?';
 connection.query(q, [data], function (error, result) {
     if (error) throw error;
 });
-*/
 
 // Selecting Data
 /*
@@ -84,17 +88,35 @@ app.get('/', function (req, res) {
     connection.query(q, function (error, results) {
         if (error) throw error;
         const count = results[0].count;
-        res.send(`The number of user is ${count}`);
+        res.render("home", { data: count });
     });
 });
 
-app.get("/joke", function(req, res){
- let joke = "Why don't scientists trust atoms? Because they make up everything!";
- res.send(joke);
+app.post('/register', function (req, res) {
+    const person = {
+        email: req.body.email
+    };
+
+    connection.query('INSERT INTO users SET ?', person, function (err, result) {
+        if (err) throw err;
+        const q = 'SELECT COUNT(*) AS count FROM users';
+        connection.query(q, function (error, results) {
+            if (error) throw error;
+            const count = results[0].count;
+            const waitingDays = Math.floor(count / 47);
+            const waitingHours = count % 100;
+            res.send("Thanks for joining our waitlist! </br>" + "The estimated waiting time is " + waitingDays + " days and " + waitingHours + " hours");
+        });
+    });
 });
-app.get("/random_num", function(req, res){
- let num = Math.floor((Math.random() * 10) + 1);
- res.send("Your lucky number is " + num);
+
+app.get("/joke", function (req, res) {
+    let joke = "<strong>Why don't scientists trust atoms?</strong> <em>Because they make up everything!</em>";
+    res.send(joke);
+});
+app.get("/random_num", function (req, res) {
+    let num = Math.floor((Math.random() * 10) + 1);
+    res.send("Your lucky number is... </br>" + num);
 });
 
 const PORT = 3000;
